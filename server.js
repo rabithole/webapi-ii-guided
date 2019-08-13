7f1c21a5314dee5b31bdeb9c82d2275a3a159d81
Robert Kulp <pedalinon@gmail.com>
2019-08-12 20:07:57 -0700

    Server side routing

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const Hubs = require('./hubs/hubs-model.js');
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.send(`
+    <h2>Lambda Hubs API</h>
+    <p>Welcome to the Lambda Hubs API</p>
+  `);
+});
+
+
+
+// add an endpoint that returns all the messages for a hub
+// add an endpoint for adding new message to a hub
+
+module.exports = server;
